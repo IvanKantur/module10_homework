f24f3af72c746127ad26f8a18353954d8448e3c2
Ivan Kantur <ivankantur87@gmail.com>
2022-11-20 21:42:55 +0300

    main: add 3-rd task

--- task3/main.js
+++ task3/main.js
@@ -1,9 +1,105 @@
 const btn = document.querySelector('.btn');
-const inp = document.querySelector('.inp').value;
+const geo = document.getElementById('geoloc');
+const status111 = document.getElementById('status111');
+const mapLink = document.getElementById('map-link');
+const chatBody = document.querySelector('.chat-body');
 
-btn.addEventListener('click', () => {
 
-    const websocket = new WebSocket('wss://echo-ws-service.herokuapp.com');
+window.addEventListener('DOMContentLoaded', () => {
 
-    alert(`Ширина экрана : ${width} px. Высота экрана : ${height} px`);
+    // Функция, выводящая текст об ошибке
+const error = () => {
+    const text = 'Невозможно получить ваше местоположение';
+    serverMessage(text);
+}
+    
+  // Функция, срабатывающая при успешном получении геолокации
+  const success = (position) => {
+    console.log('position', position);
+    const latitude  = position.coords.latitude;
+    const longitude = position.coords.longitude;
+  
+    const mapLink = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
+    console.log(mapLink);
+    geoposition(mapLink);
+  }
+  
+  geo.addEventListener('click', () => {
+    if (!navigator.geolocation) {
+      const falure= 'Geolocation не поддерживается вашим браузером';
+      serverMessage(falure);
+    } else {
+      const succ = 'Определение местоположения…';
+      navigator.geolocation.getCurrentPosition(success, error);
+      serverMessage(succ);
+    }
+  });
+
+
+    websocket = new WebSocket('wss://echo-ws-service.herokuapp.com/');
+    websocket.onopen = function(e) {
+        sendMessage("Соединение установлено, начинайте общение");
+    };
+    websocket.onclose = function(e) {
+        sendMessage("Диалог окончен");
+        // textMessage.value = '';
+    };
+    websocket.onmessage = function(e) {
+        
+        console.log(e.data);
+        serverMessage(e.data);
+    };
+    websocket.onerror = function(e) {
+        sendMessage('Ошибка');
+    };
+
+    function serverMessage(message){
+        let newMessage = document.createElement('div');
+        newMessage.classList.add('servMessage');
+        newMessage.innerHTML = `<p classs='parag'>Ответ: ${message}</p> `;
+        chatBody.appendChild(newMessage);
+    }
+
+    function sendMessage(message){
+        let newMessage = document.createElement('div');
+        newMessage.classList.add('myMessage');
+        newMessage.innerHTML = `<p classs='parag'>${message}</p> `;
+        chatBody.appendChild(newMessage);
+        
+    }
+    function geoposition(latitude, longitude){
+        let newGeo = document.createElement('a');
+        newGeo.classList.add('myMessage');
+        // // const ssilka = document.createElement('a')
+        // newGeo.i
+        newGeo.setAttribute("href", `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`);
+        newGeo.setAttribute("target", "_blanK");
+        newGeo.textContent= 'Ваше местоположение:';
+        chatBody.appendChild(newGeo);
+    }
+
+
+
+
+    btn.addEventListener('click', () => {
+        const inputValue = document.getElementById('inp').value;
+        const textMessage = inputValue.toString();
+        if ((textMessage.trim().length == 0)||(!textMessage)) { //проверка на пустой input
+            return
+        } else {        
+            sendMessage(inputValue);
+            websocket.send(inputValue);
+        }
+        
+        
+
+        document.getElementById('inp').value = ' ';
+    })
+
+    // geo.addEventListener('click', ()=>{
+        
+    // })
+
+
+    
 })
